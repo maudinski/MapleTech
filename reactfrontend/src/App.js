ee22592be2619ce6af31e811aef527bbbfd80fae
Mauricio Martinez <mauriciomartinez@Mauricios-MacBook-Air.local>
2022-05-24 16:18:29 -0500

    pre material ui

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,20 +1,49 @@
-import Header from "./Header"
-import MainBody from "./MainBody"
+import { BrowserRouter, Route, Link, Routes} from "react-router-dom";
+import CwkComponent from "./pages/cwk/CwkComponent"
+import LeechComponent from "./pages/leech/LeechComponent"
+import WashingComponent from "./pages/washing/WashingComponent"
+import FourOhFourComponent from "./pages/404/FourOhFourComponent"
+import HomeComponent from "./pages/home/HomeComponent"
+import RollingComponent from "./pages/rolling/RollingComponent"
 
 
-function App() {
-  // it would make most sense to have this all within the main body class,
-  // then pass the main body class down to the NavBar, which passes down to each NavBarButton,
-  // then each button calls mainBody.changeState(mainBody.cwk_state) or something
+export default function App() {
 
+	return (
+		<>
+		<h1>MapleTech</h1>
+		<BrowserRouter forceRefresh={true}>
+			<div>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/cwk">Cwk</Link>
+					</li>
+					<li>
+						<Link to="/leech">Leech Calculator</Link>
+					</li>
+					<li>
+						<Link to="/washing">Washing Calculator</Link>
+					</li>
+					<li>
+						<Link to="/roll">Mass Roll</Link>
+					</li>
+				</ul>
 
-  return (
-    <>
-    <Header></Header>
-    <br></br>
-    <MainBody></MainBody>
-    </>
-  )
+				<hr />
+				<Routes>
+					<Route exact path="/" element={<HomeComponent/>}/>
+					<Route exact path="/cwk" element={<CwkComponent/>} />
+					<Route exact path="/leech" element={<LeechComponent/>} />
+					<Route exact path="/washing" element={<WashingComponent/>} />
+					<Route exact path="/roll" element={<RollingComponent/>} />
+					<Route exact path="*" element={<FourOhFourComponent/>} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+		</>
+
+	)
 }
-
-export default App;
